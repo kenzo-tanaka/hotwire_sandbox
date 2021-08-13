dc19f5ff54ef3a1dc04a517471a5c45ae9578e34
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-13 21:13:57 +0900

    Add message edit page.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_room, only: %i[new create]
+  before_action :set_room, only: %i[new create edit update]
 
   def new
     @message = @room.messages.new
@@ -7,6 +7,19 @@ class MessagesController < ApplicationController
 
   def create
     @message = @room.messages.create!(message_params)
+
+    respond_to do |format|
+      format.html { redirect_to @room }
+    end
+  end
+
+  def edit
+    @message = Message.find(params[:id])
+  end
+
+  def update
+    @message = Message.find(params[:id])
+    @message.update(message_params)
 
     respond_to do |format|
       format.html { redirect_to @room }
