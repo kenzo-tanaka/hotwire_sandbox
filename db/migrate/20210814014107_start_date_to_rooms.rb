b926c98e8a7975676a365f6faaa8ad4d329009a6
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-14 10:46:49 +0900

    Add start_time column to rooms table and display on the calendar.

---
+++ db/migrate/20210814014107_start_date_to_rooms.rb
@@ -0,0 +1,5 @@
+class StartDateToRooms < ActiveRecord::Migration[6.1]
+  def change
+    add_column :rooms, :start_time, :date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_114944) do
+ActiveRecord::Schema.define(version: 2021_08_14_014107) do
 
   create_table "messages", force: :cascade do |t|
     t.integer "room_id", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_08_13_114944) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "start_time"
   end
 
   add_foreign_key "messages", "rooms"
